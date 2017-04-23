a09b8fe842dfedf4e330542bd2a8760b3d222578
Anuradha Bhatt <anudave21@gmail.com>
2017-04-23 09:52:59 -0500

    Initial commit and Intsallling and Using Vue

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      task1: 'Vaccum Living Room',
+      task2: 'Complete HW',
+      task3: 'Yada yada'
+    }
+  });
+});
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,5 @@
+class TasksController < ApplicationController
+  def item
+    render "home.html.erb"
+  end
+end
